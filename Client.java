732d3539b63a7d41d0a6f02a53e13f54298bbb72
user <singhakash.101998@gmail.com>
2023-04-05 16:57:17 +0530

    Adding gui

--- Client.java
+++ Client.java
@@ -1,15 +1,34 @@
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
-public class Client {
+
+public class Client extends JFrame {
 
     Socket socket;
 
     BufferedReader br;
     PrintWriter out;
+
+    //Declare Component
+    private JLabel heading = new JLabel("Client Area");
+    private JTextArea messageArea = new JTextArea();
+    private JTextField messageInput = new JTextField();
+    private Font font = new Font("Roboto",Font.PLAIN,20);
 
     //constructor
     public Client(){
@@ -21,12 +40,77 @@ public class Client {
             br= new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out= new PrintWriter(socket.getOutputStream());
 
+
+            createGUI();
+            handleEvents();
             startReading();
-            startWriting();
+            // startWriting();
 
         } catch (Exception e) {
             // TODO: handle exception
         }
+    }
+
+    private void handleEvents(){
+        messageInput.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                // TODO Auto-generated method stub
+                // throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+                // System.out.println("key released"+e.getKeyCode());
+                if (e.getKeyCode()==10) {
+                    System.out.println("You have pressed enter button..");
+                    String contentToSend=messageInput.getText();
+                    messageArea.append("Me :"+contentToSend+"\n");
+                    out.println(contentToSend);
+                    out.flush();
+                    messageInput.setText("");
+                    messageInput.requestFocus();
+                }
+            }
+            
+        });
+    }
+
+
+    private void createGUI(){
+        //gui code.
+        this.setTitle("Client Mesager[END]");
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //coding for component
+        heading.setFont(font);
+        messageArea.setFont(font);
+        messageInput.setFont(font);
+
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        messageArea.setEditable(false);
+        //frame layout
+        this.setLayout(new BorderLayout());
+
+        //adding components to frame
+        this.add(heading,BorderLayout.NORTH);
+        JScrollPane jScrollPane=new JScrollPane(messageArea);
+        this.add(jScrollPane,BorderLayout.CENTER);
+        this.add(messageInput,BorderLayout.SOUTH);
+
+        this.setVisible(true);
     }
 
     public void startReading(){
@@ -39,9 +123,12 @@ public class Client {
                 String msg =br.readLine();
                 if(msg.equals("exit")){
                     System.out.println("Server terminated the chat");
+                    JOptionPane.showMessageDialog(this, "Server terminated the chat");
+                    messageInput.setEnabled(false);
                     socket.close();
                     break;
                 }
+                messageArea.append("Server : "+msg+"\n");
                 System.out.println("Server : "+msg);
             
        
